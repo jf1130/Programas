4210aa4c1272ee5f24b34a0d390a749a35deef60
jf1130 <78433329+jf1130@users.noreply.github.com>
2021-04-20 13:09:55 -0500

    ejercicio7

---
+++ Ejercicio7.py
@@ -0,0 +1,6 @@
+nEM =  int(input('ingrese el numero de empleados'))
+kHO =  int(input('ingrese el numero de horas'))
+
+k = kHO * 1
+
+print("necesita " + str(nEM) + " empleados para hacer la actividad en " + str(k) + " horas")
